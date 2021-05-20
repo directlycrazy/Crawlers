71511eb855ab5171b2f6aa13ff3af6a1415f70d1
directlycrazy <33164679+directlycrazy@users.noreply.github.com>
2021-05-20 19:06:43 -0400

    fix autocomplete in search bar

--- src/routes/autocomplete.js
+++ src/routes/autocomplete.js
@@ -6,7 +6,8 @@ const router = express.Router();
 router.get('/', (req, res) => {
 	if (req.query.q) {
 		axios.get('https://suggestqueries.google.com/complete/search?output=toolbar&hl=en&q=' + req.query.q).then((a) => {
-			return res.send(a.data.replace('<?xml version="1.0"?>', '').replace('<toplevel>', '[').replace('</toplevel>', ']').replace(/<CompleteSuggestion><suggestion data=/g, "").replace(/[/]><[/]CompleteSuggestion>/g, ', ').replace(', ]', ']'))
+			var a = a.data.replace('<?xml version="1.0"?>', '').replace('<toplevel>', '[').replace('</toplevel>', ']').replace(/<CompleteSuggestion><suggestion data=/g, "").replace(/[/]><[/]CompleteSuggestion>/g, ', ').replace(', ]', ']')
+			return res.json([req.query.q, JSON.parse(a)])
 		}).catch((e) => {
 			console.log(e)
 		});
