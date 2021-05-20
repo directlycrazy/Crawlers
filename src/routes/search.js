7b3f4953c58537990b59ae7a4c9caeba53a2ba97
directlycrazy <33164679+directlycrazy@users.noreply.github.com>
2021-05-20 17:47:22 -0400

    bang support

--- src/routes/search.js
+++ src/routes/search.js
@@ -1,18 +1,49 @@
 const express = require('express');
 const google = require('google-it');
+const axios = require('axios');
+const fs = require('fs');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-	if (req.query.q) {
-		google({ 'query': req.query.q, disableConsole: true }).then(r => {
-			return res.render('search.ejs', { results: r, query: req.query.q });
-		}).catch((e) => {
-			return console.error(e);
-		});
-	} else {
-		return res.redirect('/')
-	}
+//GENERATE BANGS (uncomment below if you want to update the file on startup)
+
+/*var b = '{';
+
+axios.get('https://duckduckgo.com/bang.v255.js').then(c => {
+	var length = c.data.length;
+	Object.values(c.data).forEach((a, i) => {
+		if (a.u !== '') {
+			b += `"${a.t}" : "${a.u}", `;
+		} else {
+			length = length - 1;
+		}
+		if (i + 1 === length) {
+			b += '}';
+			fs.writeFile('bangs.json', b, () => {
+				return;
+			});
+		}
+	});
+});*/
+
+fs.readFile('bangs.json', (err, data) => {
+	if (err) return console.error(err);
+	var bangs = JSON.parse(data);
+	router.get('/', (req, res) => {
+		if (req.query.q) {
+			if (bangs[req.query.q.slice(1).split(' ')[0]]) {
+				return res.redirect(bangs[req.query.q.split(' ')[0].slice(1)].replace('{{{s}}}', req.query.q.replace(req.query.q.split(' ')[0] + ' ', "")))
+			} else {
+				google({ 'query': req.query.q, disableConsole: true }).then(r => {
+					return res.render('search.ejs', { results: r, query: req.query.q });
+				}).catch((e) => {
+					return console.error(e);
+				});
+			}
+		} else {
+			return res.redirect('/');
+		}
+	});
 });
 
 module.exports = router;
