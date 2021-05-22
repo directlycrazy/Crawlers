88767cdfcc9e42d5c0a926370283b75a28e17a26
directlycrazy <33164679+directlycrazy@users.noreply.github.com>
2021-05-22 11:46:46 -0400

    remove logs

--- src/routes/knowledge.js
+++ src/routes/knowledge.js
@@ -6,7 +6,6 @@ const router = express.Router();
 router.get('/', (req, res) => {
 	if (req.query.q) {
 		axios.get(`https://api.duckduckgo.com/?q=${req.query.q}&format=json`).then(a => {
-			console.log(a.data)
 			if (a.data.Abstract) {
 				return res.json({
 					description: a.data.Abstract,
@@ -19,7 +18,7 @@ router.get('/', (req, res) => {
 					heading: a.data.Heading,
 					source: a.data.AbstractSource,
 					url: a.data.AbstractURL
-				})
+				});
 			} else if (a.data.Answer) {
 				return res.json({
 					answer: a.data.Answer,
