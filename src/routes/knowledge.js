a6b962267a18b17e2921f57af6b212b5f9b8dc6a
directlycrazy <33164679+directlycrazy@users.noreply.github.com>
2021-05-21 23:17:48 -0400

    more useful knowledge panels

--- src/routes/knowledge.js
+++ src/routes/knowledge.js
@@ -6,12 +6,24 @@ const router = express.Router();
 router.get('/', (req, res) => {
 	if (req.query.q) {
 		axios.get(`https://api.duckduckgo.com/?q=${req.query.q}&format=json`).then(a => {
+			console.log(a.data)
 			if (a.data.Abstract) {
-				res.json({
+				return res.json({
 					description: a.data.Abstract,
 					source: a.data.AbstractSource,
 					url: a.data.AbstractURL,
 					heading: a.data.Heading
+				});
+			} else if (a.data.Heading) {
+				return res.json({
+					heading: a.data.Heading,
+					source: a.data.AbstractSource,
+					url: a.data.AbstractURL
+				})
+			} else if (a.data.Answer) {
+				return res.json({
+					answer: a.data.Answer,
+					answer_type: a.data.AnswerType
 				});
 			} else {
 				return res.sendStatus(404);
