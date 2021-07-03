e21516915452a0a9d2a6ebb233e9079ef2c99741
directlycrazy <33164679+directlycrazy@users.noreply.github.com>
2021-07-03 14:24:36 -0400

    Fixes #22 some results have empty links

--- src/scrape.js
+++ src/scrape.js
@@ -48,11 +48,23 @@ class Scrape {
 					var result_title = a.data ? a.data : a.children[0].data;
 					var result_link = $('div.ZINbbc > div:nth-child(1) > a')[link_index].attribs.href;
 					var result_snippet = $('#main > div > div > div > div:not(.v9i61e) > div.AP7Wnd')[i];
+
 					if (!(result_link.startsWith('/url?esrc=') || result_link.startsWith('http://www.google.com/url?esrc=') || result_link.startsWith('https://www.google.com/url?esrc='))) {
 						link_index++;
-						result_link = $('div.ZINbbc > div:nth-child(1) > a')[link_index].attribs.href;
+						var c = $('div.ZINbbc > div:nth-child(1) > a')[link_index].attribs;
+						if (c) {
+							return result_link = c.href;
+						} else {
+							return result_link = null;
+						}
 					}
-					results.push({ title: result_title, link: link(result_link), snippet: snippet(result_snippet) });
+
+					var b = { title: result_title, link: link(result_link), snippet: snippet(result_snippet) };
+
+					if (b.title && b.link && b.snippet) {
+						results.push(b);
+					}
+
 					link_index++;
 				});
 
