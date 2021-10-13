8de668e7fa8c16bda984a5ea040661a8c2539d55
directlycrazy <33164679+directlycrazy@users.noreply.github.com>
2021-10-13 15:35:54 -0400

    Prevent bang queries from being sent to Google

--- src/routes/autocomplete.js
+++ src/routes/autocomplete.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
 	if (req.query.q) {
+		if (req.query.q.startsWith('!')) return res.json(["", []]);
 		axios.get('https://suggestqueries.google.com/complete/search?output=toolbar&hl=en&q=' + encodeURIComponent(req.query.q)).then((a) => {
 			try {
 				var a = a.data.replace('<?xml version="1.0"?>', '').replace('<toplevel>', '[').replace('</toplevel>', ']').replace(/<CompleteSuggestion><suggestion data=/g, "").replace(/[/]><[/]CompleteSuggestion>/g, ', ').replace(', ]', ']');
